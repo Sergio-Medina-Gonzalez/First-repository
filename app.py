9197d6a1e1ffcee19addb98359bbb73485bc89ea
Dr. Sergio Medina-González <102832113+Sergio-Medina-Gonzalez@users.noreply.github.com>
2022-04-01 14:41:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title('My first wonderful app')
 st.sidebar.button('Click here')
 st.balloons()
-st.sider('Pick a number', min=10, max=1000)
+st.sider('Pick a number', 10, 1000)
